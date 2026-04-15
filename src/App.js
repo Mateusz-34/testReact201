7c74635c5ea172f6fb273dda9dbe8080d5ef29fd
Mateusz Ostróżka <mostrozka@technikumlodz.pl>
2026-04-15 12:19:02 +0200

    Dodanie dwoch plikow w folderze components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Hello from './components/Hello';
+import Hello2 from './components/Hello2';
 
 function App() {
   return (
     <div className="App">
       <Hello/>
+      <Hello2/>
     </div>
   );
 }
